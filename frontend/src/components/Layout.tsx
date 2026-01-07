4d8a688f48c47003e7840f5fe7bbb0ee63c1159f
FaishalLunawan <faishallunawan@gmail.com>
2026-01-07 15:12:28 +0700

    Initial commit

---
+++ frontend/src/components/Layout.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import Header from './Header';
+
+interface LayoutProps {
+  children: React.ReactNode;
+}
+
+export default function Layout({ children }: LayoutProps) {
+  return (
+    <div className="min-h-screen bg-gray-50">
+      <Header />
+      <div className="flex">
+        <main className="flex-1 p-6">
+          <div className="max-w-7xl mx-auto">
+            {children}
+          </div>
+        </main>
+      </div>
+    </div>
+  );
+}
